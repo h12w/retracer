78ff40c9264db64ca840bb0e883a86c093f02e5a
Hǎiliàng Wáng <w@h12.me>
2016-08-25 19:01:58 +0800

    fix HTTPS tracing; refactor

--- js.go
+++ js.go
@@ -2,7 +2,9 @@ package retracer
 
 import (
 	"bufio"
+	"bytes"
 	"io"
+	"io/ioutil"
 	"net"
 	"net/http"
 	"net/http/httptest"
@@ -20,49 +22,100 @@ type JSTracer struct {
 	Certs   CertPool
 }
 
-func (t *JSTracer) Trace(uri string, body []byte) (string, error) {
-	redirectChan := make(chan string)
-	errChan := make(chan error)
+func isResource(uri string) bool {
+	switch strings.ToLower(path.Ext(uri)) {
+	case ".js", ".css", ".png", ".gif", ".jpg", ".jpeg":
+		return true
+	}
+	return false
+}
 
-	proxy := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		if req.Method == "GET" {
-			if req.RequestURI == uri {
-				w.Write(body)
-			} else {
-				switch strings.ToLower(path.Ext(req.RequestURI)) {
-				case ".js", ".css", ".png", ".gif", ".jpg", ".jpeg":
-					return
-				}
-				redirectChan <- req.RequestURI
-			}
-		} else if req.Method == "CONNECT" {
-			host := req.URL.Host
-			cli, err := hijack(w)
-			if err != nil {
-				errChan <- err
-				return
-			}
-			defer cli.Close()
-			if err := OK200(cli); err != nil {
-				errChan <- err
-				return
-			}
-			conn, err := fakeSecureConn(cli, trimPort(host), t.Certs)
-			if err != nil {
-				errChan <- err
-				return
-			}
-			defer conn.Close()
+type fakeProxy struct {
+	uri          string
+	body         []byte
+	certs        CertPool
+	redirectChan chan string
+	errChan      chan error
+	proxy        *httptest.Server
+}
 
-			tlsReq, err := http.ReadRequest(bufio.NewReader(conn))
-			if err != nil {
-				errChan <- err
-				return
-			}
-			redirectChan <- "https://" + req.RequestURI + tlsReq.RequestURI
+func newProxy(uri string, body []byte, certs CertPool) *fakeProxy {
+	fp := &fakeProxy{
+		uri:          uri,
+		body:         body,
+		certs:        certs,
+		redirectChan: make(chan string),
+		errChan:      make(chan error),
+	}
+
+	fp.proxy = httptest.NewServer(http.HandlerFunc(fp.serve))
+	return fp
+}
+
+func (p *fakeProxy) serveHTTP(w http.ResponseWriter, req *http.Request) {
+	if req.RequestURI == p.uri {
+		w.Write(p.body)
+	} else {
+		if !isResource(req.RequestURI) {
+			p.redirectChan <- req.RequestURI
 		}
-	}))
-	defer proxy.Close()
+	}
+}
+
+func (p *fakeProxy) serve(w http.ResponseWriter, req *http.Request) {
+	if req.Method == "GET" {
+		p.serveHTTP(w, req)
+	} else if req.Method == "CONNECT" {
+		host := req.URL.Host
+		cli, err := hijack(w)
+		if err != nil {
+			p.errChan <- err
+			return
+		}
+		defer cli.Close()
+		if err := OK200(cli); err != nil {
+			p.errChan <- err
+			return
+		}
+		conn, err := fakeSecureConn(cli, trimPort(host), p.certs)
+		if err != nil {
+			p.errChan <- err
+			return
+		}
+		defer conn.Close()
+
+		tlsReq, err := http.ReadRequest(bufio.NewReader(conn))
+		if err != nil {
+			p.errChan <- err
+			return
+		}
+		requestURI := "https://" + trimPort(req.RequestURI) + tlsReq.RequestURI
+		if requestURI == p.uri {
+			resp := http.Response{
+				Status:     http.StatusText(http.StatusFound),
+				StatusCode: http.StatusFound,
+				Proto:      "HTTP/1.1",
+				ProtoMajor: 1,
+				ProtoMinor: 1,
+				Body:       ioutil.NopCloser(bytes.NewReader(p.body)),
+				Close:      true,
+			}
+			if err := resp.Write(conn); err != nil {
+				p.errChan <- err
+			}
+		} else {
+			if !isResource(requestURI) {
+				p.redirectChan <- requestURI
+			}
+		}
+	}
+}
+
+func (t *JSTracer) Trace(uri string, body []byte) (string, error) {
+	proxy := newProxy(uri, body, t.Certs)
+	redirectChan := proxy.redirectChan
+	errChan := proxy.errChan
+	defer proxy.proxy.Close()
 
 	browser := exec.Command(
 		"surf",
@@ -71,7 +124,7 @@ func (t *JSTracer) Trace(uri string, body []byte) (string, error) {
 		uri)
 	browser.Env = []string{
 		"DISPLAY=" + os.Getenv("DISPLAY"),
-		"http_proxy=" + proxy.URL,
+		"http_proxy=" + proxy.proxy.URL,
 	}
 	defer func() {
 		if browser.Process != nil {
